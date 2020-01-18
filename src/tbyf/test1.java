550cd901402e271a3b838f106be05499d6e0dcf8
287681392 <287681392@qq.com>
2020-01-18 15:28:51 +0800

    b

---
+++ src/tbyf/test1.java
@@ -0,0 +1,17 @@
+package tbyf;
+
+import javax.print.DocFlavor;
+
+public class test1 {
+    public static void main(String[] args) {
+        String A;
+        String b;
+        String c;
+        A = "1";
+        b= "r";
+        c = "3";
+        System.out.println(A);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
